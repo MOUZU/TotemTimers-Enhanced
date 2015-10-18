eabfaced18db07dfaacd9022aec0122da79c60e7
aka LYQ / Virose <mouzugaming@outlook.de>
2015-10-18 23:59:15 +0200

    Initial upload

---
+++ TotemTimer.lua
@@ -0,0 +1,154 @@
+local _G = getfenv(0)
+
+local Dragging = 0;
+
+function TotemTimer_OnUpdate(arg1)
+	if( this.element ) then
+		local data = TTActiveTotems[this.element];
+		if ( data and data.active ) then
+			if ( data.duration < 0 ) then
+				TotemTimers_TotemDeath(data.totem);
+			else
+				data.duration = data.duration - arg1;
+				getglobal(this:GetName().."Time"):SetText(TotemTimer_FormatTime(data.duration));
+				if( TTData.Totems[data.totem] ) then
+					if( data.warningTime ) then
+						if ( data.duration <= data.warningTime ) then
+							data.warningTime = nil;
+							TotemTimers_TotemWarn(data.totem);
+						end
+					end
+				end
+				if( data.duration <= 60 ) then
+					getglobal(this:GetName().."Time"):SetTextColor(1.0,1.0,1.0);
+				else
+					getglobal(this:GetName().."Time"):SetTextColor(1.0,.8,0);
+				end
+				if( data.duration <= TTData.flash ) then
+					data.flashTime = data.flashTime - arg1;
+					if ( data.flashTime < 0 ) then
+						if ( data.flashState == 1 ) then
+							data.flashState = 0;
+							data.flashTime = BUFF_FLASH_TIME_OFF;
+						else
+							data.flashState = 1;
+							data.flashTime = BUFF_FLASH_TIME_ON;
+						end
+					end
+					if ( data.flashState == 1 ) then
+						totemAlphaValue = (BUFF_FLASH_TIME_ON - data.flashTime) / BUFF_FLASH_TIME_ON;
+						totemAlphaValue = totemAlphaValue * (1 - BUFF_MIN_ALPHA) + BUFF_MIN_ALPHA
+					else
+						totemAlphaValue = data.flashTime / BUFF_FLASH_TIME_ON;
+						totemAlphaValue = (data.flashTime * (1 - BUFF_MIN_ALPHA)) + BUFF_MIN_ALPHA;
+						getglobal(this:GetName().."Icon"):SetAlpha(data.flashTime / BUFF_FLASH_TIME_ON);
+					end
+					getglobal(this:GetName().."Icon"):SetAlpha(totemAlphaValue);
+				end
+                
+                -- update tick here
+                if data.ticks and data.ticks[1] then
+                    if data.ticks[1]<= data.duration then
+                        local dif = data.duration - data.ticks[1]
+                        dif = math.floor(10*dif)/10
+                        if not IsAddOnLoaded("!OmniCC") then
+                            -- i noticed that omnicc is overwriting the cooldown template and creates a timer itself
+                            if _G[this:GetName().."Tick"]:GetText() ~= dif then
+                                getglobal(this:GetName().."Tick"):SetText(dif)
+                            end
+                        end
+                    else
+                        tremove(TTActiveTotems[this.element].ticks,1)
+                        --if not _G[this:GetName().."Cooldown"]:IsVisible() then
+                            CooldownFrame_SetTimer(_G[this:GetName().."Cooldown"], GetTime(), TotemData[data.totem].tick, 1)
+                        --end
+                    end
+                end
+                
+			end
+		end
+	end
+end
+
+function TotemTimer_ReCast()
+	if( TTData.lock == 1 ) then
+		local data = TTActiveTotems[this.element];
+		--DEFAULT_CHAT_FRAME:AddMessage(TTActiveTotems[this.element]);
+		--DEFAULT_CHAT_FRAME:AddMessage(data);
+		if( data ) then
+			if( data["action"] ) then
+				UseAction(data["action"], data["number"], data["target"]);
+			elseif( data["spell"] ) then
+				CastSpell(data["spell"], data["book"]);
+			elseif ( data["Slot_Id"] ) then
+				--DEFAULT_CHAT_FRAME:AddMessage(data["Slot_Id"])
+				UseInventoryItem( data["Slot_Id"] );
+			end
+		end
+	end
+end
+
+function TotemTimer_OnDragStart()
+	if( TTData.lock ~= 1 ) then
+		TotemTimersFrame:StartMoving();
+	end
+end
+
+function TotemTimer_OnDragStop()
+	TotemTimersFrame:StopMovingOrSizing();
+end
+
+function TotemTimer_OnEnter()
+	local data = TTActiveTotems[this.element];
+	if( data ) then
+		if ( this:GetCenter() < UIParent:GetCenter() ) then
+			GameTooltip:SetOwner(this, "ANCHOR_RIGHT");
+		else
+			GameTooltip:SetOwner(this, "ANCHOR_LEFT");
+		end
+		if( data["action"] ) then
+			GameTooltip:SetAction(data["action"]);
+		elseif( data["spell"] ) then
+			GameTooltip:SetSpell(data["spell"], data["book"]);
+		elseif( data["Slot_Id"] ) then
+			GameTooltip:SetInventoryItem("player", data["Slot_Id"]);
+		end
+	end
+end
+
+function TotemTimer_OnLoad()
+	this:RegisterForClicks("LeftButtonUp","RightButtonUp");
+end
+
+-- Functions ripped from TotemTimer
+function TotemTimer_FormatTime(seconds)
+  local d, h, m, s;
+  local text;
+
+ 	if( TTData[TT_TIME] == TT_BLIZZARD ) then
+		if(seconds <= 0) then
+			text = "";
+		elseif(seconds < 60) then
+			d, h, m, s = ChatFrame_TimeBreakDown(seconds);
+			text = format("%d s", s);
+		elseif(seconds < 3600) then
+			d, h, m, s = ChatFrame_TimeBreakDown(seconds);
+			text = format("%d m", m);
+		else
+			text = "1 hr+";
+		end
+	else
+		if(seconds <= 0) then
+			text = "";
+		elseif(seconds < 3600) then
+			d, h, m, s = ChatFrame_TimeBreakDown(seconds);
+			text = format("%02d:%02d", m, s);
+		else
+			text = "1 hr+";
+		end
+	end
+
+  return text;
+end
+
+
---
+++ TotemTimersData.lua
@@ -0,0 +1,144 @@
+-- TotemData This information must be included to work properly.
+TotemData = {};
+--  icon        = file name of the icon
+--  duration    = duration in seconds
+--  tick        = the time between ticks in seconds
+
+TotemData[TT_DISEASE_CLEANSING] = {
+    icon        = "Spell_Nature_DiseaseCleansingTotem",
+    duration    = 120,
+    tick        = 5,
+};
+
+TotemData[TT_EARTHBIND] = {
+    icon        = "Spell_Nature_StrengthOfEarthTotem02",
+    duration    = 45,
+    tick        = 3,
+};
+
+TotemData[TT_FIRE_NOVA] = {
+    icon        = "Spell_Fire_SealOfFire",
+    duration    = 5,
+};
+
+TotemData[TT_FIRE_RESISTANCE] = {
+    icon        = "Spell_FireResistanceTotem_01",
+    duration    = 120,
+};
+
+TotemData[TT_FLAMETONGUE] = {
+    icon        = "Spell_Nature_GuardianWard",
+    duration    = 120,
+    tick        = 5,
+};
+
+TotemData[TT_FROST_RESISTANCE] = {
+    icon        = "Spell_FrostResistanceTotem_01",
+    duration    = 120,
+};
+
+TotemData[TT_GRACE_OF_AIR] = {
+    icon        = "Spell_Nature_InvisibilityTotem",
+    duration    = 120,
+};
+
+TotemData[TT_GROUNDING] = {
+    icon        = "Spell_Nature_GroundingTotem",
+    duration    = 45,
+};
+
+TotemData[TT_HEALING_STREAM] = {
+    icon        = "INV_Spear_04",
+    duration    = 60,
+    --tick        = 2,
+};
+
+TotemData[TT_MAGMA] = {
+    icon        = "Spell_Fire_SelfDestruct",
+    duration    = 20,
+    tick        = 2,
+};
+
+TotemData[TT_MANA_SPRING] = {
+    icon        = "Spell_Nature_ManaRegenTotem",
+    duration    = 60,
+    --tick        = 2, I'm commenting this out because I've had weird test results ranging from 1s to 3s
+};
+
+TotemData[TT_MANA_TIDE] = {
+    icon        = "Spell_Frost_SummonWaterElemental",
+    duration    = 12,
+    tick        = 3,
+};
+
+TotemData[TT_ANCIENT_MANA_SPRING] = {
+    -- LYQ: dafuq is dis?
+    icon        = "INV_Wand_01",
+    duration    = 24,
+    --tick        = 5,
+};
+
+TotemData[TT_NATURE_RESISTANCE] = {
+    icon        = "Spell_Nature_NatureResistanceTotem",
+    duration    = 120,
+};
+
+TotemData[TT_POISON_CLEANSING] = {
+    icon        = "Spell_Nature_PoisonCleansingTotem",
+    duration    = 120,
+    tick        = 5,
+};
+
+TotemData[TT_SEARING] = {
+    icon        = "Spell_Fire_SearingTotem",
+    --tick        = 2, I'm commenting the tick out because the tick works here like a cooldown
+    -- rank[index] equals duration in this case
+    rank        = {30,35,40,45,50,55},
+};
+
+TotemData[TT_SENTRY] = {
+    icon        = "Spell_Nature_RemoveCurse",
+    duration    = 300,
+};
+
+TotemData[TT_STONECLAW] = {
+    icon        = "Spell_Nature_StoneClawTotem",
+    duration    = 15,
+    tick        = 1.5,
+    -- rank[index] equals hitpoints in this case
+    rank        = {206,276,316,346,426,486},
+};
+
+TotemData[TT_STONESKIN] = {
+    icon        = "Spell_Nature_StoneSkinTotem",
+    duration    = 120,
+};
+
+TotemData[TT_STRENGTH_OF_EARTH] = {
+    icon        = "Spell_Nature_EarthBindTotem",
+    duration    = 120,
+};
+
+TotemData[TT_TREMOR] = {
+    icon        = "Spell_Nature_TremorTotem",
+    duration    = 120,
+    tick        = 4,
+};
+
+TotemData[TT_TRANQUIL_AIR] = {
+    icon        = "Spell_Nature_Brilliance",
+    duration    = 120,
+};
+
+TotemData[TT_WINDFURY] = {
+    icon        = "Spell_Nature_Windfury",
+    duration    = 120,
+    tick        = 5,
+};
+
+TotemData[TT_WINDWALL] = {
+    icon        = "Spell_Nature_EarthBind",
+    duration    = 120,
+};
+
+TT_EMPTY_ICON = "Spell_Totem_WardOfDraining";
